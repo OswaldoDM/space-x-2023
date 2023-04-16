31ef168e673532789b3b1aabc0e57489fafec09a
Oswaldo <oswaldoe1992@gmail.com>
2023-04-16 13:30:24 -0400

    odm

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,21 +7,18 @@ return(
 <>
 <section className="absolute w-full">
 
-    <div className="container mx-auto px-4 md:px-6 lg:px-8 xl:px-0 pt-5 flex justify-between items-center">       
+    <div className="container mx-auto px-5 sm:px-6 lg:px-8 xl:px-0 pt-5 flex justify-between items-center">       
         
         
-        <div>
-            <img 
-            src={logo} 
-            alt='space x logo'
-            loading="lazy"
-            className="w-[200px] lg:w-[250px] h-auto"/>
-        </div>
+        <a  href='#' target='_blank' 
+            className="font-blanka text-[42px] sm:text-5xl text-white opacity-[0.95]">
+                ODM
+        </a>
         
 
         <a 
         href="#contact" 
-        className="text-white text-[14px] font-dinbold border-effect">
+        className="text-white text-[14px] font-dinbold border-effect pt-4 sm:pt-5">
             CONTACT
         </a>
         
